890fdba878494236eea8f9d051daf628e2632b48
hkavya <148327751+hkavya460@users.noreply.github.com>
2024-05-02 18:34:35 +0530

    Update Batch_graident.py

--- Batch_graident.py
+++ Batch_graident.py
@@ -2,24 +2,27 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
-
+#Loading the data (supervised linear regression data) 
 df = pd.read_csv("simulated_data_multiple_linear_regression_for_ML.csv")
 #print(df)
 print(df.shape)
+#x they all are features(Independent varibales) y is dependent variables 
 x = df[["age","BMI","BP","blood_sugar","Gender"]]
-#print(x)
 y = df["disease_score_fluct"]
 
-
+# Hypothesis Function  = theta.Transpose * X
 
 def hypothesis(x,theta):
     h_funct= np.dot(x,theta)
     return h_funct
+#Cost function is nothing but sum of square of errors  square of difference between the actual y - predicted y 
+#our aim is to reduce the cost function the difference or erorr should be low 
 def cost_function(h_funct,y):
     error = h_funct - y
     m = len(y)
     sse = np.sum(error **2) / (2 *m)
     return sse
+    #Gradient descent is parameter update, update the theta untill it leads to global minima (reduce the cost function)
 def gradient_descent(x,y,alpha,num_itera):
     theta = np.zeros(x.shape[1])
     cost =[]
